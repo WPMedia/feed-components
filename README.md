18e3d22347fd710393549e75817c425f41563e28
Cameron Woodmansee <cameron.woodmansee@washpost.com>
2020-03-02 17:51:01 -0500

    initial commit
    add single package
    add babel config

---
+++ README.md
@@ -0,0 +1,4 @@
+# Feeds
+
+The repo contains the work to migrate partner feeds to fusion compatible blocks. Mono repo workflow managed using tools from this [Monorepo Guide](https://monorepo.guide/).
+
---
+++ packages/output-type-sitemap/src/index.js
@@ -0,0 +1,34 @@
+import builder from 'xmlbuilder2'
+
+const CHANGE_FREQ = 'always'
+const PRIORITY = '0.5'
+
+const makeElement = (el) => ({
+  loc: el.canonical_url,
+  ...(el.last_updated_date && { lastmode: el.last_updated_date }),
+  changefeq: CHANGE_FREQ,
+  priority: PRIORITY,
+  'image:image': getImages(el)
+})
+
+const getImages = (el) => {
+  let images = el.content_elements.filter(e => e.type === 'image')
+  return images.map(i => ({ 'image:loc': i.url }))
+}
+
+const Json = ({ children, globalContent, ...props }) => {
+  const { content_elements: elements } = globalContent
+
+  return builder.create({
+    urlset: {
+      '@xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
+      '@xmlns:image': 'http://www.google.com/schemas/sitemap-image/1.1',
+      url: elements.map(makeElement)
+    }
+  })
+}
+
+// Specify content type
+Json.contentType = 'application/xml'
+
+export default Json
