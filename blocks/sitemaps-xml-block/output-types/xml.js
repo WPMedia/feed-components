e0644a7dbe99eda3b2f521928bf454781e2e7755
ibeyene <iyobas@gmail.com>
2020-04-07 15:45:02 -0400

    Update package.json

--- blocks/sitemaps-xml-block/output-types/xml.js
+++ blocks/sitemaps-xml-block/output-types/xml.js
@@ -1,7 +1,7 @@
 import { create } from 'xmlbuilder2'
 
 const XmlOutput = ({ children }) => {
-  console.log({children})
+  console.log('children are ', children[0])
   return children ? create(children[0]).end({ pretty: true }) : null
 }
 
