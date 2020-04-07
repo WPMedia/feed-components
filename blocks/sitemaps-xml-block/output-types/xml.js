e1f606f81bca9f0c96a7d82645e89c33cdcbc7f0
ibeyene <iyobas@gmail.com>
2020-04-07 17:32:35 -0400

    use commonjs for output type

--- blocks/sitemaps-xml-block/output-types/xml.js
+++ blocks/sitemaps-xml-block/output-types/xml.js
@@ -1,10 +1,10 @@
-import { create } from 'xmlbuilder2'
+const xmlBuilder = require('xmlbuilder2')
 
 const XmlOutput = ({ children }) => {
-  console.log('children are ', children[0])
-  return children ? create(children[0]).end({ pretty: true }) : null
+
+  return children ? xmlBuilder.create(children[0]).end({ pretty: true }) : null
 }
 
 XmlOutput.contentType = 'application/xml'
 
-export default XmlOutput
+module.exports = XmlOutput
