b2ae0ef469e10c5f2e53002d96ae6880f957d0a4
Timothy Kosmider <timothy.kosmider@washpost.com>
2020-04-15 13:39:32 -0400

    sitemap-index initial commit AIO-29

---
+++ blocks/sitemap-index-feature-block/features/sitemap-index/xml.test.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import Consumer from 'fusion:consumer'
+import { SitemapIndex } from './xml'
+
+it('returns template with default values', () => {
+  const sitemapindex = SitemapIndex({
+    arcSite: 'the-globe',
+    globalContent: {
+      count: 437,
+      content_elements: [
+        {
+          last_updated_date: '2020-04-07T15:02:08.918Z',
+          website_url: '/food/2020/04/07/tips-for-safe-hand-washing',
+        },
+      ],
+    },
+    customFields: {
+      feedPath: 'arcio/sitemap',
+      lastMod: 'last_updated_date',
+    },
+  })
+  expect(sitemapindex).toMatchSnapshot()
+})
