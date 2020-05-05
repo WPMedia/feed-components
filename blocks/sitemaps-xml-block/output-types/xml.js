bfd3c5c94b225d8d4aca35f64341a38a149fb386
Cameron Woodmansee <cameron.woodmansee@washpost.com>
2020-05-05 18:05:08 -0400

    remove output type block

--- blocks/sitemaps-xml-block/output-types/xml.js
+++
@@ -1,9 +0,0 @@
-const xmlBuilder = require('xmlbuilder2')
-
-const XmlOutput = ({ children }) => {
-  return children ? xmlBuilder.create(children[0]).end({ pretty: true }) : null
-}
-
-XmlOutput.contentType = 'application/xml'
-
-module.exports = XmlOutput
