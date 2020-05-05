04fd3fb3e81276c6d677ac81cd5ed00754c7f65c
Timothy Kosmider <timothy.kosmider@washpost.com>
2020-05-05 12:01:33 -0400

    Update output type AIO-111
    - add encoding-UTF8
    - Removed pretty print
    - Add invalidCharReplacement to strip out invalid utf-8 characters.  We
    have issues from time to time with clients copy/pasting in bad
    characters

--- blocks/sitemaps-xml-block/output-types/xml.js
+++ blocks/sitemaps-xml-block/output-types/xml.js
@@ -1,7 +1,12 @@
 const xmlBuilder = require('xmlbuilder2')
 
 const XmlOutput = ({ children }) => {
-  return children ? xmlBuilder.create(children[0]).end({ pretty: true }) : null
+  return children
+    ? xmlBuilder
+        .create(children[0], { invalidCharReplacement: '' })
+        .dec({ encoding: 'UTF-8' })
+        .end()
+    : null
 }
 
 XmlOutput.contentType = 'application/xml'
