dffee1420c22302cdec0a7bfaeb65979ce7d6bc7
Cameron Woodmansee <cameron29w@gmail.com>
2020-05-15 17:14:06 -0400

    refactor sitemap blocks to use shared modules (#60)

---
+++ .changeset/selfish-rats-flash.md
@@ -0,0 +1,7 @@
+---
+'@wpmedia/sitemap-feature-block': minor
+'@wpmedia/sitemap-news-feature-block': minor
+'@wpmedia/sitemap-video-feature-block': minor
+---
+
+rename from plural (sitemaps) to singular (sitemap) and refactor to rely on shared modules
--- blocks/sitemap-feature-block/features/sitemap/xml.js
+++ blocks/sitemap-feature-block/features/sitemap/xml.js
@@ -3,6 +3,8 @@ import Consumer from 'fusion:consumer'
 import get from 'lodash/get'
 import getProperties from 'fusion:properties'
 import { resizerKey } from 'fusion:environment'
+import { buildResizerURL } from '@wpmedia/feeds-resizer'
+import { generatePropsForFeed } from '@wpmedia/feeds-prop-types'
 
 const sitemapTemplate = (
   elements,
@@ -13,7 +15,7 @@ const sitemapTemplate = (
     lastMod,
     imageTitle,
     imageCaption,
-    getImgURL,
+    resizerURL,
     domain,
   },
 ) => ({
@@ -35,7 +37,7 @@ const sitemapTemplate = (
         ...(includePromo &&
           img && {
             'image:image': {
-              'image:loc': getImgURL(s),
+              'image:loc': buildResizerURL(img.url, resizerKey, resizerURL),
               ...(img[imageCaption] && {
                 'image:caption': { $: img[imageCaption] },
               }),
@@ -52,36 +54,10 @@ const sitemapTemplate = (
 export function Sitemap({ globalContent, customFields, arcSite }) {
   const { resizerURL = '', feedDomainURL = '' } = getProperties(arcSite)
 
-  const getImgURL = (element) => {
-    const buildURL = (_url) => {
-      if (typeof window === 'undefined') {
-        const Thumbor = require('thumbor-lite')
-        const thumbor = new Thumbor(resizerKey, resizerURL)
-        let imgSrc = _url.replace(/^http[s]?:\/\//, '').replace(' ', '%20')
-        if (imgSrc.includes('?')) {
-          imgSrc = imgSrc.replace('?', '%3F')
-        }
-
-        return thumbor.setImagePath(imgSrc).resize(1200, 630).buildUrl()
-      }
-      return null
-    }
-
-    if (
-      element.promo_items &&
-      element.promo_items.basic &&
-      element.promo_items.basic.url
-    ) {
-      return buildURL(element.promo_items.basic.url)
-    }
-
-    return ''
-  }
-
   // can't return null for xml return type, must return valid xml template
   return sitemapTemplate(get(globalContent, 'content_elements', []), {
     ...customFields,
-    getImgURL,
+    resizerURL,
     domain: feedDomainURL,
   })
 }
@@ -102,58 +78,7 @@ Sitemap.propTypes = {
         'ANS value for associated story image used for the <image:caption> sitemap tag',
       defaultValue: 'caption',
     }),
-    lastMod: PropTypes.oneOf([
-      'created_date',
-      'display_date',
-      'first_publish_date',
-      'last_updated_date',
-      'publish_date',
-    ]).tag({
-      label: 'Last Modified Date',
-      group: 'Format',
-      description: 'Which date field should be used in the sitemap',
-      defaultValue: 'last_updated_date',
-    }),
-    changeFreq: PropTypes.oneOf([
-      'always',
-      'hourly',
-      'daily',
-      'weekly',
-      'monthly',
-      'yearly',
-      'never',
-      'Exclude from sitemap',
-    ]).tag({
-      label: 'change frequency',
-      group: 'Format',
-      description: 'What is the Change frequency of the sitemap',
-      defaultValue: 'always',
-    }),
-    includePromo: PropTypes.boolean.tag({
-      label: 'Include promo images?',
-      group: 'Format',
-      description: 'Include an image in the sitemap',
-      defaultValue: true,
-    }),
-    priority: PropTypes.oneOf([
-      '0.0',
-      '0.1',
-      '0.2',
-      '0.3',
-      '0.4',
-      '0.5',
-      '0.6',
-      '0.7',
-      '0.8',
-      '0.9',
-      '1.0',
-      'Exclude from sitemap',
-    ]).tag({
-      label: 'priority',
-      group: 'Format',
-      description: 'What is the priority of the sitemap',
-      defaultValue: '0.5',
-    }),
+    ...generatePropsForFeed('sitemap', PropTypes),
   }),
 }
 Sitemap.label = 'Standard Sitemap'
