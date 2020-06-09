4fad969fcbebe6c68ddc8ae22d3e5abb975f8a8b
Timothy Kosmider <timothy.kosmider@washpost.com>
2020-06-09 12:05:13 -0400

    Add find-video-stream code to repo

---
+++ utils/find-video-stream/src/findVideo.js
@@ -0,0 +1,29 @@
+const jmespath = require('jmespath')
+
+export function formatSearchObject(searchObject) {
+  /* convert Object into a string usable by jmespath
+   * {key: value}
+   * ["key == `value`"]
+   */
+  const keys = Object.keys(searchObject)
+  const jsmSearcher = keys.reduce((acc, key) => {
+    return [...acc, `${key} == \`${searchObject[key]}\``]
+  }, [])
+
+  return jsmSearcher
+}
+
+export function findVideo(video, searchObject) {
+  /* search video.streams for first video encoding that matches key, values */
+  if (video && video.streams && searchObject) {
+    const searchString = formatSearchObject(searchObject)
+    const videoStreams = jmespath.search(
+      video,
+      'streams[?'.concat(searchString.join('&&'), ']'),
+    )
+
+    if (videoStreams.length) {
+      return videoStreams[0]
+    }
+  }
+}
