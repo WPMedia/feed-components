62bd40e2885f65c76fef0410c2f23a3cd1c6d27c
sarukalva <sarukalva@gmail.com>
2020-08-25 09:34:37 -0400

    added new video-content api

---
+++ blocks/feeds-source-video-api-block/sources/feeds-video-api.js
@@ -0,0 +1,41 @@
+import { VIDEO_BASE } from 'fusion:environment'
+
+const resolve = function resolve(key) {
+  let requestUri, uriParams
+  let feedQuery = []
+  if (key['uuid']) {
+    requestUri = `${VIDEO_BASE}/api/v1/ansvideos/findByUuids`
+    uriParams = [`uuids=${key['uuids']}`]
+
+    if (key['uuids']) {
+      try {
+        feedQuery = JSON.parse(key['uuids'])
+      } catch (error) {
+        console.log(`Failed to parse uuid: ${key['uuids']}`)
+      }
+    }
+  } else {
+    requestUri = `${VIDEO_BASE}/api/v1/ans/playlists/findByPlaylist`
+    uriParams = [`name=${key['name']}`, `count=${key['count']}`].join('&')
+
+    if (key['count']) {
+      try {
+        feedQuery = JSON.parse(key['count'])
+      } catch (error) {
+        console.log(`Failed to parse count: ${key['count']}`)
+      }
+    }
+  }
+
+  const encodedBody = encodeURI(JSON.stringify(body))
+  return `${requestUri}?body=${encodedBody}&${uriParams}`
+}
+
+export default {
+  resolve,
+  schemaName: 'feeds',
+  params: {
+    Playlist: 'text',
+    Count: 'text',
+  },
+}
