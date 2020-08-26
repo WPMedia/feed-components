60c097cb13481c38dcb1cc09954f4ef22d59e4a3
sarukalva <sarukalva@gmail.com>
2020-08-26 13:23:33 -0400

    Revert "yarn tests working"
    This reverts commit 9e5e1070ba78c57a0698b31437d0ee492ed8cdb2.
    
    go back

--- blocks/feeds-source-video-api-block/sources/feeds-video-api.js
+++ blocks/feeds-source-video-api-block/sources/feeds-video-api.js
@@ -2,17 +2,18 @@ import { VIDEO_BASE } from 'fusion:environment'
 
 const resolve = function resolve(key) {
   let requestUri, uriParams
-  if (key) {
-    if (key && key.uuids) {
-      requestUri = `${VIDEO_BASE}/api/v1/ansvideos/findByUuids`
-      uriParams = [`uuids=${key.uuids}`]
-    } else {
-      let count
-      !key.count ? (count = 10) : (count = key.count)
-      requestUri = `${VIDEO_BASE}/api/v1/ans/playlists/findByPlaylist`
-      uriParams = [`name=${key.name}`, `count=${count}`].join('&')
+  if (key.uuids) {
+    requestUri = `${VIDEO_BASE}/api/v1/ansvideos/findByUuids`
+    uriParams = [`uuids=${key.uuids}`]
+  } else {
+    let count = key.count
+    if (!key.count) {
+      count = 10
     }
+    requestUri = `${VIDEO_BASE}/api/v1/ans/playlists/findByPlaylist`
+    uriParams = [`name=${key.name}`, `count=${count}`].join('&')
   }
+
   return `${requestUri}?${uriParams}`
 }
 
@@ -22,6 +23,6 @@ export default {
   params: {
     Uuids: 'text',
     Playlist: 'text',
-    Count: 'text',
+    count: 'text',
   },
 }
