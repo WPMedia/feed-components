3d825114939d73046cd64738d42b73e868665594
sarukalva <sarukalva@gmail.com>
2020-08-26 11:39:36 -0400

    updated readme and tester

--- blocks/feeds-source-video-api-block/sources/feeds-video-api.test.js
+++ blocks/feeds-source-video-api-block/sources/feeds-video-api.test.js
@@ -1,15 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import Consumer from 'fusion:consumer'
 const resolver = require('./feeds-video-api')
-
 it('validate schemaName', () => {
   expect(resolver.default.schemaName).toBe('feeds')
 })
+
+const query1 = resolver.default.resolve()
 it('returns query with default values', () => {
-  const query = resolver.default.resolve()
-  expect(query).toBe('undefined/api/v1/ansvideos/findByUuids')
+  expect(query1).toBe('undefined/api/v1/ansvideos/findByUuids')
+  const query1 = resolver.default.resolve({
+    uuids: 'a,b,c',
+  })
 })
+expect(query1).toBe('undefined/api/v1/ansvideos/findByUuids?uuids=a,b,c')
+
+const query2 = resolver.default.resolve()
 it('returns query with default values', () => {
-  const query = resolver.default.resolve()
-  expect(query).toBe('undefined/api/v1/ansvideos/findByPlaylist')
+  expect(query2).toBe('undefined/api/v1/ansvideos/findByPlaylist')
+  const query2 = resolver.default.resolve({
+    name: 'playlist',
+    count: '10',
+  })
 })
+expect(query2).toBe(
+  'undefined/api/v1/ansvideos/findByPlaylist?name=playlist&count=10',
+)
