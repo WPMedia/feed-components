63efae513aefbd8941920f1f77ddc5611d5bc699
Timothy Kosmider <timothy.kosmider@washpost.com>
2021-02-25 17:16:27 -0500

    Update collection CS
    - Inflate collection content so it can be used in RSS
    - Hardcode ttl : 300
    - Add IncludedFields for the ids call.  By default it will not return
    content_elements, it will include the following if left blank
       'content_elements',
       'created_date',
       'credits',
       'description',
       'display_date',
       'first_publish_date',
       'headlines',
       'last_updated_date',
       'promo_items',
       'publish_date',
       'subheadlines',
       'taxonomy',
       'website_url',

--- blocks/feeds-source-collections-block/sources/collections.test.js
+++ blocks/feeds-source-collections-block/sources/collections.test.js
@@ -1,36 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import Consumer from 'fusion:consumer'
-const resolver = require('./collections')
+const fetch = require('./collections')
 
 it('validate params', () => {
-  expect(resolver.default.params).toStrictEqual({
+  expect(fetch.default.params).toStrictEqual({
     _id: 'text',
     content_alias: 'text',
     from: 'text',
     size: 'text',
+    includedFields: 'text',
+    ttl: 'number',
   })
-})
-
-it('returns query search by _id', () => {
-  const query = resolver.default.resolve({
-    _id: 'NBXKZJC3ONEGRB2VPINBMQTNFA',
-    content_alias: '',
-    from: '',
-    size: '',
-  })
-  expect(query).toBe(
-    'content/v4/collections?_id=NBXKZJC3ONEGRB2VPINBMQTNFA&published=true',
-  )
-})
-
-it('returns query by alias', () => {
-  const query = resolver.default.resolve({
-    _id: '',
-    content_alias: 'sports page',
-    from: '5',
-    size: '10',
-  })
-  expect(query).toBe(
-    'content/v4/collections?content_alias=sports page&from=5&size=10&published=true',
-  )
 })
