34bcb5bbf60e05694429c892d5184a9019509fdd
Timothy Kosmider <timothy.kosmider@washpost.com>
2021-03-19 14:13:37 -0400

    Sort collection   AIO-566
    Results from Content-API ids call are in _id order, not the order they
    were requested.  Reorder the results to match collection order
    Added descriptions to the parameters
    Mock collection and C-API calls
    Add core-js to handle test with async call

--- blocks/feeds-source-collections-block/sources/collections.test.js
+++ blocks/feeds-source-collections-block/sources/collections.test.js
@@ -1,13 +1,113 @@
-// eslint-disable-next-line no-unused-vars
-import Consumer from 'fusion:consumer'
+import request from 'request-promise-native'
 const fetch = require('./collections')
 
+jest.mock('request-promise-native')
+
 it('validate params', () => {
-  expect(fetch.default.params).toStrictEqual({
-    _id: 'text',
-    content_alias: 'text',
-    from: 'text',
-    size: 'text',
-    included_fields: 'text',
+  expect(fetch.default.params).toStrictEqual([
+    { displayName: 'Collection ID', name: '_id', type: 'text' },
+    {
+      displayName: 'Collection Alias (Only populate ID or Alias)',
+      name: 'content_alias',
+      type: 'text',
+    },
+    {
+      displayName: 'From - Integer offset to start from',
+      name: 'from',
+      type: 'text',
+    },
+    {
+      displayName: 'Number of records to return, Integer 1 - 20',
+      name: 'size',
+      type: 'text',
+    },
+    {
+      displayName: 'ANS Fields to include, use commas between fields',
+      name: 'include_fields',
+      type: 'text',
+    },
+  ])
+})
+
+describe('collections', () => {
+  describe('fetch', () => {
+    beforeEach(() => {
+      request.mockClear()
+    })
+
+    it('Get collection', (done) => {
+      request.mockResolvedValueOnce({
+        type: 'collection',
+        content_elements: [
+          {
+            _id: 'AAAAAAAAAAAAAAAAAAAAAAAAAA',
+            website_url: '/storyA',
+          },
+          {
+            _id: 'BBBBBBBBBBBBBBBBBBBBBBBBBB',
+            website_url: '/storyB',
+          },
+          {
+            _id: 'CCCCCCCCCCCCCCCCCCCCCCCCCC',
+            website_url: '/storyC',
+          },
+          {
+            _id: 'DDDDDDDDDDDDDDDDDDDDDDDDDD',
+            website_url: '/storyD',
+          },
+        ],
+      })
+
+      request.mockResolvedValueOnce({
+        type: 'results',
+        content_elements: [
+          {
+            _id: 'BBBBBBBBBBBBBBBBBBBBBBBBBB',
+            website_url: '/storyB',
+          },
+          {
+            _id: 'AAAAAAAAAAAAAAAAAAAAAAAAAA',
+            website_url: '/storyA',
+          },
+          {
+            _id: 'DDDDDDDDDDDDDDDDDDDDDDDDDD',
+            website_url: '/storyD',
+          },
+          {
+            _id: 'CCCCCCCCCCCCCCCCCCCCCCCCCC',
+            website_url: '/storyC',
+          },
+        ],
+      })
+
+      fetch.default
+        .fetch({
+          id: 'id',
+        })
+        .then((response) => {
+          expect(response).toEqual({
+            type: 'collection',
+            content_elements: [
+              {
+                _id: 'AAAAAAAAAAAAAAAAAAAAAAAAAA',
+                website_url: '/storyA',
+              },
+              {
+                _id: 'BBBBBBBBBBBBBBBBBBBBBBBBBB',
+                website_url: '/storyB',
+              },
+              {
+                _id: 'CCCCCCCCCCCCCCCCCCCCCCCCCC',
+                website_url: '/storyC',
+              },
+              {
+                _id: 'DDDDDDDDDDDDDDDDDDDDDDDDDD',
+                website_url: '/storyD',
+              },
+            ],
+          })
+          done()
+        })
+    })
   })
 })
