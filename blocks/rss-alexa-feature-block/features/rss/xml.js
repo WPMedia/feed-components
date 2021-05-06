29bc6462570821dac5719c193594753901dceac6
Malavika Koppula <mkreddy1110@gmail.com>
2021-05-06 11:09:19 -0400

    Created Alexa feed component

---
+++ blocks/rss-alexa-feature-block/features/rss/xml.js
@@ -0,0 +1,183 @@
+import PropTypes from 'fusion:prop-types'
+import Consumer from 'fusion:consumer'
+import get from 'lodash/get'
+import moment from 'moment'
+import getProperties from 'fusion:properties'
+import { resizerKey } from 'fusion:environment'
+import { BuildContent } from '@wpmedia/feeds-content-elements'
+import { generatePropsForFeed } from '@wpmedia/feeds-prop-types'
+import { buildResizerURL } from '@wpmedia/feeds-resizer'
+
+// const jsdom = require('jsdom')
+
+const jmespath = require('jmespath')
+
+const rssTemplate = (
+  elements,
+  {
+    channelTitle,
+    channelDescription,
+    channelPath,
+    channelCopyright,
+    channelTTL,
+    channelCategory,
+    channelLogo,
+    imageTitle,
+    imageCaption,
+    imageCredits,
+    itemTitle,
+    pubDate,
+    itemCategory,
+    includePromo,
+    includeContent,
+    resizerURL,
+    resizerWidth,
+    resizerHeight,
+    domain,
+    feedTitle,
+    feedLanguage,
+    rssBuildContent,
+  },
+) => ({
+  rss: {
+    '@version': '2.0',
+    channel: {
+      title: { $: channelTitle || feedTitle },
+      link: `${domain}`,
+      description: { $: channelDescription || `${feedTitle} News Feed` },
+      lastBuildDate: moment
+        .utc(new Date())
+        .format('ddd, DD MMM YYYY HH:mm:ss ZZ'),
+      ...(feedLanguage && { language: feedLanguage }),
+      ...(channelCategory && { category: channelCategory }),
+      ...(channelCopyright && {
+        copyright: channelCopyright,
+      }), // TODO Add default logic
+      ...(channelTTL && { ttl: channelTTL }),
+      ...(channelLogo && {
+        image: {
+          url: buildResizerURL(channelLogo, resizerKey, resizerURL),
+          title: channelTitle || feedTitle,
+          link: domain,
+        },
+      }),
+
+      item: elements.map((s) => {
+        let body, category
+        const url = `${domain}${s.website_url || s.canonical_url || ''}`
+        const img =
+          s.promo_items && (s.promo_items.basic || s.promo_items.lead_art)
+        return {
+          title: { $: jmespath.search(s, itemTitle) || '' },
+          link: url,
+          guid: s._id,
+          description: {
+            body: {
+              'content:encoded':
+                {
+                  $: body,
+                } || '',
+            },
+          },
+          pubDate: moment
+            .utc(s[pubDate])
+            .format('ddd, DD MMM YYYY HH:mm:ss ZZ'),
+          ...(itemCategory &&
+            (category = jmespath.search(s, itemCategory)) &&
+            category && { category: category }),
+          ...(includeContent !== 0 &&
+            (body = rssBuildContent.parse(
+              s.content_elements || [],
+              includeContent,
+              domain,
+              resizerKey,
+              resizerURL,
+              resizerWidth,
+              resizerHeight,
+            ))),
+          ...(includePromo &&
+            img &&
+            img.url && {
+              'media:content': {
+                '@type': 'image/jpeg',
+                '@url': buildResizerURL(
+                  img.url,
+                  resizerKey,
+                  resizerURL,
+                  resizerWidth,
+                  resizerHeight,
+                ),
+                ...(jmespath.search(img, imageCaption) && {
+                  'media:description': {
+                    '@type': 'plain',
+                    $: jmespath.search(img, imageCaption),
+                  },
+                }),
+                ...(jmespath.search(img, imageTitle) && {
+                  'media:title': {
+                    $: jmespath.search(img, imageTitle),
+                  },
+                }),
+                ...((jmespath.search(img, imageCredits) || []).length && {
+                  'media:credit': {
+                    '@role': 'author',
+                    '@scheme': 'urn:ebu',
+                    '#': jmespath.search(img, imageCredits).join(','),
+                  },
+                }),
+              },
+            }),
+        }
+      }),
+    },
+  },
+})
+
+export function Rss({ globalContent, customFields, arcSite }) {
+  const {
+    resizerURL = '',
+    feedDomainURL = '',
+    feedTitle = '',
+    feedLanguage = '',
+  } = getProperties(arcSite)
+  const { width = 0, height = 0 } = customFields.resizerKVP || {}
+
+  const rssBuildContent = new BuildContent()
+
+  // can't return null for xml return type, must return valid xml template
+  return rssTemplate(get(globalContent, 'content_elements', []), {
+    ...customFields,
+    resizerURL,
+    resizerWidth: width,
+    resizerHeight: height,
+    domain: feedDomainURL,
+    feedTitle,
+    feedLanguage,
+    rssBuildContent,
+  })
+}
+
+Rss.propTypes = {
+  customFields: PropTypes.shape({
+    channelPath: PropTypes.string.tag({
+      label: 'Path',
+      group: 'Channel',
+      description:
+        'Path to the feed, excluding the domain, defaults to /arc/outboundfeeds/rss',
+      defaultValue: '/arc/outboundfeeds/rss/',
+    }),
+    audioAvailable: PropTypes.kvp.tag({
+      label: 'Audio',
+      group: 'Audio',
+      description:
+        'Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo',
+      defaultValue: {
+        bitrate: 5400,
+        stream_type: 'mp3',
+      },
+    }),
+    ...generatePropsForFeed('rss', PropTypes),
+  }),
+}
+Rss.label = 'RSS Alexa'
+export default Consumer(Rss)
