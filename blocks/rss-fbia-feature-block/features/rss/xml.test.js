388a47f8bdef6172335ff7b8613b9f0b1fc03066
Timothy Kosmider <timothy.kosmider@washpost.com>
2021-09-20 13:09:55 -0400

    Add optional HxW to figure iframe  AIO-731
    Add customField iframeHxW kvp with {width:0, height:0} defaults
    Add customField wrapRawHTML bool with false default
    
    Add iframeHxW to oembed to add optional HxW to
    <figure class="op-interactive><iframe width=x height=y>
    
    Add wrapRawHTML to text, if type===raaw_html use
    <figure class="op-interactive><iframe width=x height=y>

--- blocks/rss-fbia-feature-block/features/rss/xml.test.js
+++ blocks/rss-fbia-feature-block/features/rss/xml.test.js
@@ -31,11 +31,20 @@ const articles = {
       description: { basic: 'Tips to keep you wash for 20 seconds' },
       subheadlines: { basic: 'This is from the subheadlines' },
       content_elements: [
-        { type: 'text', content: 'try singing the happy birthday song<br>' },
-        { type: 'text', content: 'be sure to <i>wash</i> your thumbs' },
         {
+          _id: 'AAAAA',
+          type: 'text',
+          content: 'try singing the happy birthday song<br>',
+        },
+        {
+          _id: 'BBBBB',
+          type: 'text',
+          content: 'be sure to <i>wash</i> your thumbs',
+        },
+        {
+          _id: 'CCCCC',
           type: 'raw_html',
-          content: `<p id="7FCQKPKS2NCH5L6YTO2XCOEDDA"><div style="background:#ffffff; color: #333; margin:100px 0 1rem 0; line-height:28px; font-family: 'Merriweather', serif;"><img style="height:120px; width: 120px; float: left; margin-right: 20px;" src="https://www.example.com.mx/graficos/img/novedad-180321.jpg" class="img-responsive" alt="logo" /><img style="height:120px; width: 120px; float: right; " src="https://example.com.mx/graficos/interior-nota/abcd_novedad.png" class="img-responsive" alt="logo" /><span style="font-size: 1.1rem; font-weight: 700;"> Concertos for Oboe, Clarinet and Orchestra </span><hr style="border: 0;  height: 1px;   background-image: -webkit-linear-gradient(left, #666666, #ffffff);  background-image: -moz-linear-gradient(left, #666666, #ffffff);  background-image: -ms-linear-gradient(left, #666666, #ffffff);  background-image: -o-linear-gradient(left,  #666666, #ffffff); padding: 0; margin: 0;"/><span style="font-size: .9rem">ARTISTA: Camerata de las Américas, dirigida por Ludwig Carrasco </span><br><span style="font-size: .9rem">SELLO:   Urtext </span><br><span style="font-size: .9rem">PRECIO:   $168n </span></div></p>`,
+          content: `<div style="background:#ffffff; color: #333; margin:100px 0 1rem 0; line-height:28px; font-family: 'Merriweather', serif;"><img style="height:120px; width: 120px; float: left; margin-right: 20px;" src="https://www.example.com.mx/graficos/img/novedad-180321.jpg" class="img-responsive" alt="logo" /><img style="height:120px; width: 120px; float: right; " src="https://example.com.mx/graficos/interior-nota/abcd_novedad.png" class="img-responsive" alt="logo" /><span style="font-size: 1.1rem; font-weight: 700;"> Concertos for Oboe, Clarinet and Orchestra </span><hr style="border: 0;  height: 1px;   background-image: -webkit-linear-gradient(left, #666666, #ffffff);  background-image: -moz-linear-gradient(left, #666666, #ffffff);  background-image: -ms-linear-gradient(left, #666666, #ffffff);  background-image: -o-linear-gradient(left,  #666666, #ffffff); padding: 0; margin: 0;"/><span style="font-size: .9rem">ARTISTA: Camerata de las Américas, dirigida por Ludwig Carrasco </span><br><span style="font-size: .9rem">SELLO:   Urtext </span><br><span style="font-size: .9rem">PRECIO:   $168n </span></div>`,
         },
         {
           type: 'oembed_response',
@@ -139,6 +148,8 @@ it('returns FB-IA template with custom values', () => {
       placementSection: '<script>myscript()</script>',
       adScripts:
         '<figure class="op-tracker"><iframe><script>alert("hi");</script></iframe></figure>',
+      iframeHxW: { width: '560' },
+      wrapRawHTML: true,
     },
   })
   expect(rss).toMatchSnapshot({
