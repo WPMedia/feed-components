70e16e5e4942ee1428f0625ac44a873182099d8b
Ryan Barnett <ryan.barnett@washpost.com>
2024-01-24 13:08:17 -0500

    fix: use standard convention; revert from consumer wrapper

--- blocks/text-output-block/output-types/text.js
+++ blocks/text-output-block/output-types/text.js
@@ -1,19 +1,18 @@
-import PropTypes from 'prop-types'
-import Consumer from 'fusion:consumer'
+import PropTypes from "prop-types";
 
-export function TextOutputType(children){
-	  const generateText = (child) => {
-    if (Array.isArray(child)) return child.map(generateText).join('\n')
-    return child
-  }
-  return generateText(children)
+const TextOutputType = ({ children }) => {
+	const generateText = (child) => {
+		if (Array.isArray(child)) return child.map(generateText).join("\n");
+		return child;
+	};
+	return generateText(children);
 };
 
 TextOutputType.contentType = "text/plain";
-
 TextOutputType.fallback = false;
 
 TextOutputType.propTypes = {
 	children: PropTypes.node,
-}
-export default Consumer(TextOutputType)
+};
+
+export default TextOutputType;
