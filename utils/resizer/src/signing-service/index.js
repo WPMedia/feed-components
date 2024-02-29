1cf97a7f046c1e2c5a39cf8d04023bf9d44b5174
Nick Schubach <nschubach@gmail.com>
2024-02-29 12:58:04 -0500

    Add rollup configs and precompile on build

--- utils/resizer/src/signing-service/index.js
+++ utils/resizer/src/signing-service/index.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios'
 import {
 	ARC_ACCESS_TOKEN,
 	CONTENT_BASE,
 	SIGNING_SERVICE_DEFAULT_APP,
 	RESIZER_TOKEN_VERSION,
-} from "fusion:environment";
-import handleFetchError from "../handle-fetch-error";
+} from 'fusion:environment'
+import handleFetchError from '../handle-fetch-error/index.js'
 
 const params = {
-	id: "text",
-	service: "text",
-	serviceVersion: "text",
-};
+	id: 'text',
+	service: 'text',
+	serviceVersion: 'text',
+}
 
 export const fetch = ({
 	id,
@@ -20,18 +20,18 @@ export const fetch = ({
 }) => {
 	const urlSearch = new URLSearchParams({
 		value: id,
-	});
+	})
 	return axios({
 		url: `${CONTENT_BASE}/signing-service/v2/sign/${service}/${serviceVersion}?${urlSearch.toString()}`,
 		headers: {
-			"content-type": "application/json",
+			'content-type': 'application/json',
 			Authorization: `Bearer ${ARC_ACCESS_TOKEN}`,
 		},
-		method: "GET",
+		method: 'GET',
 	})
 		.then(({ data: content }) => content)
-		.catch(handleFetchError);
-};
+		.catch(handleFetchError)
+}
 
 export default {
 	fetch,
@@ -39,4 +39,4 @@ export default {
 	http: false,
 	// 365 day ttl
 	ttl: 31536000,
-};
+}
